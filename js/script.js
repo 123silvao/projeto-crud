bffd3e5b411663500ab0c9fb8fff5ac96184f78d
123silvao <sc.silvio321@gmail.com>
2025-10-01 10:05:19 -0300

    terminando o crud

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
-const input = document.getElementById ("main-input")
+'use strict'
+
+const openModal = () => document.getElementById('modal')
+    .classList.add('active')
+
+const closeModal = () => document.getElementById('modal')
+    .classList.remove('active')
+
+const tempClient = {
+    nome: "Silvio",
+    email: "silvio@gmail.com",
+    celular: "1192222-2222",
+    cdade: "Jandira",
+
+}
+
+// CRUD - create read  update  delete:
+const createClient = (client) => {
+    localStorage.setItem("teste", "teste para o crud")
+}
+
+// Eventos
+
+document.getElementById('cadastrarCliente')
+    .addEventListener('click', openModal)
+
+document.getElementById('modalClose')
+    .addEventListener('click', closeModal)
